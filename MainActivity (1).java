8fa521f85aa77efee669ab2faaedc538fdf91cc6
Ekansh-Agarwal <77278829+Ekansh-Agarwal@users.noreply.github.com>
2022-05-31 08:45:12 +0530

    function added

--- MainActivity (1).java
+++ MainActivity (1).java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+//layout
     public void reset(View view)
     {
         GridLayout gridLayout = findViewById(R.id.gridLayout);
